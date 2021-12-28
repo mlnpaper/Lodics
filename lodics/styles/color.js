8e06f04c65e56ed344c6e7eb61e00cbaa7fa9bef
jihye <jjh@lodics.com>
2021-12-28 16:40:20 +0900

    style/color, font initial setting

---
+++ lodics/styles/color.js
@@ -0,0 +1,8 @@
+export const palette = {
+  themePrimary: '#0d47a1',
+  themeGray: '#aaaaaa',
+  themeWhite: '#ffffff',
+  themeBlack: '#333333',
+  themeHover: '#2871fc',
+  themeSelector: '#626f87',
+};
