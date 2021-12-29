d9242a394100c2c02d61181a3c4dfa2367a538ae
jihye <jjh@lodics.com>
2021-12-29 15:41:36 +0900

    feat/전역 상태 관리 context 생성 및 적용

---
+++ lodics/context/globalState.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import GlobalStateContext from './globalStateContext';
+
+export default function GlobalState({ children }) {
+  const [subSelectedComponent, setSubSelectedComponent] = useState('');
+
+  return (
+    <GlobalStateContext.Provider
+      value={{
+        subSelectedComponent,
+        setSubSelectedComponent,
+      }}
+    >
+      {children}
+    </GlobalStateContext.Provider>
+  );
+}
--- lodics/pages/_app.js
+++ lodics/pages/_app.js
@@ -1,15 +1,17 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import 'antd/dist/antd.css';
+import GlobalState from '../context/globalState';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <GlobalState>
       <Head>
         <meta charSet='utf-8' />
         <title>LoDiCS</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </GlobalState>
   );
 }
 
