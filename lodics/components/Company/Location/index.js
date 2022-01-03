1b6722d356ae93d760ca2e4aae4c0db5d640deca
jihye <jjh@lodics.com>
2022-01-03 14:25:55 +0900

    feat/Company 페이지 회사위치 컴포넌트 구현

--- lodics/components/Company/Location/index.js
+++ lodics/components/Company/Location/index.js
@@ -1,11 +1,135 @@
-import React from 'react';
-import { Container } from './styles';
-import { PageTitle } from '../..';
+import React, { useEffect } from 'react';
+import { BusTextContainer, MapContainer, SubwayTextContainer, TextContainer, SubwayIcon, BusIcon } from './styles';
+import { PageSemiTitle, PageTitle } from '../..';
 
 export default function Location() {
+  // <!-- 3. 실행 스크립트 -->
+  const executeScript = () => {
+    const scriptTag = document.createElement('script');
+    const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
+      "timestamp" : "1641175660053",
+      "key" : "28nsp",
+    }).render();`);
+    scriptTag.appendChild(inlineScript);
+    document.body.appendChild(scriptTag);
+  };
+
+  // <!-- 2. 설치 스크립트 * 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다. -->
+  const InstallScript = () => {
+    (function () {
+      var c = location.protocol == 'https:' ? 'https:' : 'http:';
+      var a = 'c6dc0bd8';
+
+      if (window.daum && window.daum.roughmap && window.daum.roughmap.cdn) {
+        return;
+      }
+      window.daum = window.daum || {};
+      window.daum.roughmap = {
+        cdn: a,
+        URL_KEY_DATA_LOAD_PRE: c + '//t1.daumcdn.net/roughmap/',
+        url_protocal: c,
+      };
+
+      var b = c + '//t1.daumcdn.net/kakaomapweb/place/jscss/roughmap/' + a + '/roughmapLander.js';
+
+      const scriptTag = document.createElement('script');
+      scriptTag.src = b;
+      document.body.append(scriptTag);
+      scriptTag.onload = () => {
+        executeScript();
+      };
+    })();
+  };
+
+  useEffect(() => {
+    InstallScript();
+  }, []);
+
   return (
-    <Container>
+    <React.Fragment>
       <PageTitle title='회사위치' />
-    </Container>
+      <MapContainer>
+        {/* 카카오맵 - 지도퍼가기 */}
+        {/* 1. 지도 노드  */}
+        <div id='daumRoughmapContainer1641175660053' class='root_daum_roughmap root_daum_roughmap_landing'></div>
+      </MapContainer>
+      <PageSemiTitle title='요약' />
+      <TextContainer>
+        <span>
+          지하철 2호선 뚝섬역 4번 출구, 신한은행 골목으로 들어와, 메가커피 골목으로 500m 직진.
+          <br />
+          1층 '바다화원' 건물
+        </span>
+      </TextContainer>
+      <PageSemiTitle title='주소' />
+      <TextContainer>
+        <span>서울특별시 성동구 상원4길 6-1 태양빌딩 3층 (성수동1가 14-32)</span>
+        <div>
+          <span>
+            <em>TEL</em> 02-403-1160
+          </span>
+          <span>
+            <em>FAX</em> 02-403-1108
+          </span>
+        </div>
+      </TextContainer>
+      <PageSemiTitle title='지하철역' />
+      <SubwayTextContainer>
+        <div>
+          <span>
+            뚝섬역 <SubwayIcon $color='#37B32D'>2</SubwayIcon>
+          </span>
+          <span>4번 출구</span>
+          <span>도보 2분</span>
+        </div>
+        <div>
+          <span>
+            성수역 <SubwayIcon $color='#37B32D'>2</SubwayIcon>
+          </span>
+          <span>1번 출구</span>
+          <span>도보 10분</span>
+        </div>
+        <div>
+          <span>
+            서울숲역 <SubwayIcon $color='#E9AF17'>분당</SubwayIcon>
+          </span>
+          <span>1번 출구</span>
+          <span>도보 13분</span>
+        </div>
+      </SubwayTextContainer>
+      <PageSemiTitle title='버스정류장' />
+      <BusTextContainer>
+        <div>
+          <span>뚝섬역4번출구</span>
+          <span>(04239) | 30m</span>
+          <span>
+            <BusIcon $color='#6372D7'>간선</BusIcon>N62(심야)
+          </span>
+          <span>
+            <BusIcon $color='#53A135'>지선</BusIcon>2016 | 2224 | 2413
+          </span>
+        </div>
+        <div>
+          <span>뚝섬역3번5번출구</span>
+          <span>(04238) | 70m</span>
+          <span>
+            <BusIcon $color='#6372D7'>간선</BusIcon>N62(심야)
+          </span>
+          <span>
+            <BusIcon $color='#53A135'>지선</BusIcon>2016 | 2224 | 2413
+          </span>
+        </div>
+        <div>
+          <span>뚝섬역2번출구</span>
+          <span>(04237) | 198m</span>
+          <span>
+            <BusIcon $color='#6372D7'>간선</BusIcon>N62(심야)
+          </span>
+          <span>
+            <BusIcon $color='#53A135'>지선</BusIcon>2016 | 2224 | 2413
+          </span>
+        </div>
+      </BusTextContainer>
+    </React.Fragment>
   );
 }
--- lodics/components/Company/Location/styles.js
+++ lodics/components/Company/Location/styles.js
@@ -1,5 +1,134 @@
 import styled from 'styled-components';
+import { palette } from '../../../styles/color';
 
-export const Container = styled.div`
-  border: 1px solid blue;
+export const MapContainer = styled.div`
+  width: 100%;
+  height: calc(100vh - 35rem);
+  margin-bottom: 10rem;
+
+  #daumRoughmapContainer1641175660053 {
+    width: 100%;
+    height: 100%;
+    overflow: visible;
+
+    .wrap_map {
+      height: 100%;
+    }
+  }
+`;
+
+export const TextContainer = styled.div`
+  margin-top: 2rem;
+  padding-bottom: 5rem;
+  border-bottom: 1px dashed ${palette.themeDashedBorder};
+  line-height: 2.5rem;
+
+  span {
+    color: ${palette.themeBlack};
+    font-size: 1.6rem;
+  }
+
+  em {
+    color: ${palette.themeBlack};
+    font-size: 1.6rem;
+    font-weight: 600;
+  }
+
+  div {
+    margin-top: 1rem;
+    span {
+      margin-right: 2rem;
+    }
+  }
+`;
+
+export const SubwayTextContainer = styled.div`
+  margin-top: 5rem;
+  padding-bottom: 5rem;
+  border-bottom: 1px dashed ${palette.themeDashedBorder};
+  display: flex;
+  flex-flow: row nowarp;
+  align-items: center;
+
+  div {
+    display: flex;
+    flex-flow: row nowarp;
+    align-items: center;
+    margin-right: 5rem;
+
+    span {
+      color: ${palette.themeBlack};
+      font-size: 1.6rem;
+      margin-right: 0.5rem;
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+
+      &:last-child {
+        color: red;
+        margin-left: 0.5rem;
+      }
+    }
+  }
+`;
+
+export const SubwayIcon = styled.i`
+  color: ${palette.themeWhite};
+  margin-left: 0.5rem;
+  margin-right: 2rem;
+  display: inline-block;
+  padding: 0.2rem 0.75rem;
+  border-radius: 50%;
+  background: ${props => props.$color};
+  position: relative;
+
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    display: block;
+    margin-left: 1rem;
+    width: 1px;
+    height: 13px;
+    background-color: ${palette.themeGray};
+    top: 50%;
+    right: -1rem;
+    transform: translateY(-50%);
+  }
+`;
+
+export const BusTextContainer = styled.div`
+  margin-top: 5rem;
+  padding-bottom: 5rem;
+  display: flex;
+  flex-flow: row nowrap;
+
+  div {
+    display: flex;
+    flex-flow: column;
+    margin-right: 5rem;
+
+    span {
+      color: ${palette.themeBlack};
+      font-size: 1.6rem;
+      line-height: 3rem;
+      display: flex;
+      align-items: center;
+
+      &:nth-child(2) {
+        color: #666666;
+      }
+    }
+  }
+`;
+
+export const BusIcon = styled.i`
+  background: ${props => props.$color};
+  color: ${palette.themeWhite};
+  border-radius: 5px;
+  padding: 0.2rem 0.5rem;
+  height: 2rem;
+  line-height: 1.6rem;
+  margin-right: 0.5rem;
 `;
