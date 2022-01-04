ec06e47a22452fe7851a91a7f73e833dff978c46
jihye <jjh@lodics.com>
2022-01-04 16:07:52 +0900

    style/SubMenu 반응형에 따른 border style 적용

--- lodics/components/SubMenu/styles.js
+++ lodics/components/SubMenu/styles.js
@@ -7,24 +7,35 @@ export const Container = styled.div`
   /* -------------------------------------------------------------------------- */
   /*                               모바일  & default                             */
   /* -------------------------------------------------------------------------- */
-  height: 30rem;
+  height: 20rem;
   background: center / cover no-repeat url('img/basic_bg.jpg');
   width: 100%;
 
   h2 {
     color: ${palette.themeWhite};
-    font-size: 7rem;
+    font-size: 3rem;
     font-weight: 600;
     text-align: center;
-    line-height: 30rem;
+    line-height: 20rem;
     margin: 0;
+    word-break: keep-all;
+    backdrop-filter: grayscale(30%);
+  }
+
+  @media all and (min-width: 768px) {
+    /* -------------------------------------------------------------------------- */
+    /*                                   테블릿                                    */
+    /* -------------------------------------------------------------------------- */
+    height: 30rem;
+
+    h2 {
+      font-size: 7rem;
+      line-height: 30rem;
+    }
   }
 `;
 
 export const MenuContainer = styled.div`
-  /* -------------------------------------------------------------------------- */
-  /*                               모바일  & default                             */
-  /* -------------------------------------------------------------------------- */
   height: 5rem;
   width: 100%;
   background: ${palette.themePrimary};
@@ -34,32 +45,37 @@ export const MenuInnerContainer = styled.div`
   /* -------------------------------------------------------------------------- */
   /*                               모바일  & default                             */
   /* -------------------------------------------------------------------------- */
-
-  width: calc(100% - 5rem);
+  width: 100%;
   height: 100%;
   margin: 0 auto;
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
   color: ${palette.themeWhite};
+  overflow: hidden;
 
   i {
     width: 50px;
     height: 50px;
-    padding: 1.3rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     cursor: pointer;
     border-right: 1px solid ${palette.themeWhite};
-    border-left: 1px solid ${palette.themeWhite};
     color: ${palette.themeWhite};
 
     svg {
-      width: 100%;
-      height: 100%;
+      width: 23px;
+      height: 23px;
     }
   }
 
   .ant-select {
     height: 100%;
+
+    &:last-child .ant-select-selector {
+      border-right: none;
+    }
 
     .ant-select-selector {
       height: 100%;
@@ -77,21 +93,37 @@ export const MenuInnerContainer = styled.div`
 
     .ant-select-selection-item {
       height: 100%;
-      font-size: 1.4rem;
+      font-size: 1.2rem;
       font-family: 'Nanum Gothic';
-      padding: 0.8rem 2rem 0;
+      padding: 0.8rem 3rem 0 1rem;
       color: ${palette.themeWhite};
     }
 
     .ant-select-arrow {
       color: ${palette.themeWhite};
-      padding-right: 2rem;
+      padding-right: 1.5rem;
     }
   }
+
   @media all and (min-width: 768px) {
     /* -------------------------------------------------------------------------- */
     /*                                   테블릿                                    */
     /* -------------------------------------------------------------------------- */
+    width: calc(100% - 5rem);
+
+    i {
+      border-left: 1px solid ${palette.themeWhite};
+    }
+
+    .ant-select {
+      &:last-child .ant-select-selector {
+        border-right: 1px solid ${palette.themeWhite};
+      }
+
+      .ant-select-selection-item {
+        font-size: 1.4rem;
+      }
+    }
   }
 
   @media all and (min-width: 1200px) {
