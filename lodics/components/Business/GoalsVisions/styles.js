0ca4741b30f4e8545f7b83aa83d1866c2c6c68d5
jihye <jjh@lodics.com>
2022-01-04 15:32:04 +0900

    style/Business 페이지 반응형 적용

--- lodics/components/Business/GoalsVisions/styles.js
+++ lodics/components/Business/GoalsVisions/styles.js
@@ -2,16 +2,56 @@ import styled from 'styled-components';
 import { palette } from '../../../styles/color';
 
 export const Container = styled.div`
+  /* -------------------------------------------------------------------------- */
+  /*                               모바일  & default                             */
+  /* -------------------------------------------------------------------------- */
   border-bottom: 1px solid ${palette.themeDashedBorder};
   padding-bottom: 10rem;
 
   p {
-    font-size: 1.6rem;
+    font-size: 1.3rem;
     margin: 0;
   }
 
   &:last-child {
     border: none;
+  }
+
+  @media all and (min-width: 768px) {
+    /* -------------------------------------------------------------------------- */
+    /*                                   테블릿                                    */
+    /* -------------------------------------------------------------------------- */
+    border-bottom: 1px solid ${palette.themeDashedBorder};
+    padding-bottom: 10rem;
+
+    p {
+      font-size: 1.6rem;
+    }
+  }
+`;
+
+export const ImgContainer = styled.div`
+  /* -------------------------------------------------------------------------- */
+  /*                               모바일  & default                             */
+  /* -------------------------------------------------------------------------- */
+  width: 100%;
+
+  img {
+    border: 1px solid ${palette.themeDashedBorder};
+    display: block;
+    width: 100%;
+    min-height: 50vh;
+  }
+
+  @media all and (min-width: 768px) {
+    /* -------------------------------------------------------------------------- */
+    /*                                   테블릿                                    */
+    /* -------------------------------------------------------------------------- */
+    padding: 0 5rem;
+
+    img {
+      min-height: 100vh;
+    }
   }
 `;
 
@@ -24,23 +64,23 @@ export const InnerContainer = styled.div`
 `;
 
 export const GeointProcessContainer = styled(InnerContainer)`
+  /* -------------------------------------------------------------------------- */
+  /*                               모바일  & default                             */
+  /* -------------------------------------------------------------------------- */
   img {
     display: block;
     border: 1px solid ${palette.themeDashedBorder};
     width: 100%;
-    min-height: 100vh;
+    min-height: 50vh;
   }
-`;
 
-export const ImgContainer = styled.div`
-  width: 100%;
-  padding: 0 5rem;
-
-  img {
-    border: 1px solid ${palette.themeDashedBorder};
-    display: block;
-    width: 100%;
-    min-height: 100vh;
+  @media all and (min-width: 768px) {
+    /* -------------------------------------------------------------------------- */
+    /*                                   테블릿                                    */
+    /* -------------------------------------------------------------------------- */
+    img {
+      min-height: 100vh;
+    }
   }
 `;
 
@@ -50,8 +90,11 @@ export const EGovernmentImgContainer = styled.div`
 `;
 
 export const ThirdTitle = styled.span`
+  /* -------------------------------------------------------------------------- */
+  /*                               모바일  & default                             */
+  /* -------------------------------------------------------------------------- */
   display: inline-block;
-  font-size: 2rem;
+  font-size: 1.6rem;
   font-weight: 600;
   position: relative;
   padding-left: 3rem;
@@ -63,10 +106,23 @@ export const ThirdTitle = styled.span`
     display: block;
     top: 50%;
     left: 0;
-    width: 15px;
-    height: 15px;
-    border: 5px solid ${palette.themePrimary};
+    width: 13px;
+    height: 13px;
+    border: 4px solid ${palette.themePrimary};
     transform: translateY(-50%);
+  }
+
+  @media all and (min-width: 768px) {
+    /* -------------------------------------------------------------------------- */
+    /*                                   테블릿                                    */
+    /* -------------------------------------------------------------------------- */
+    font-size: 2rem;
+
+    &:after {
+      width: 15px;
+      height: 15px;
+      border: 5px solid ${palette.themePrimary};
+    }
   }
 `;
 
@@ -98,37 +154,57 @@ export const ContentContainer = styled.div`
 `;
 
 export const ImgCardContainer = styled.div`
+  /* -------------------------------------------------------------------------- */
+  /*                               모바일  & default                             */
+  /* -------------------------------------------------------------------------- */
   display: flex;
   flex-flow: row wrap;
   margin-top: 2rem;
   position: relative;
-  padding: 0 5rem;
 
   &:after {
     content: '';
     position: absolute;
     display: block;
-    top: 48%;
+    top: 49%;
     left: 50%;
     transform: translateX(-50%);
     width: 80px;
     height: 80px;
-    border-top: 50px solid #dddddd;
-    border-bottom: 50px solid none;
-    border-right: 50px solid transparent;
-    border-left: 50px solid transparent;
+    border-top: 30px solid #dddddd;
+    border-bottom: 30px solid none;
+    border-right: 40px solid transparent;
+    border-left: 40px solid transparent;
+  }
+
+  @media all and (min-width: 768px) {
+    /* -------------------------------------------------------------------------- */
+    /*                                   테블릿                                    */
+    /* -------------------------------------------------------------------------- */
+    padding: 0 5rem;
+
+    &:after {
+      top: 48%;
+      border-top: 50px solid #dddddd;
+      border-bottom: 50px solid none;
+      border-right: 50px solid transparent;
+      border-left: 50px solid transparent;
+    }
   }
 `;
 
 export const ImgCardInnerContainer = styled.div`
+  /* -------------------------------------------------------------------------- */
+  /*                               모바일  & default                             */
+  /* -------------------------------------------------------------------------- */
   margin: 0 1rem;
   border: 1px solid ${palette.themeDashedBorder};
   width: ${props => props.$width};
-  padding: 2rem 2rem 0 2rem;
+  padding: 1rem 1rem 0 1rem;
   border-radius: 10px;
   display: flex;
   flex-flow: column;
-  margin-bottom: 20rem;
+  margin-bottom: 10rem;
 
   &:last-child {
     margin-bottom: 0;
@@ -137,13 +213,31 @@ export const ImgCardInnerContainer = styled.div`
   img {
     border-radius: 10px;
     border: 1px solid ${palette.themeDashedBorder};
-    min-height: 500px;
+    min-height: 160px;
     width: 100%;
   }
 
   span {
-    font-size: 2.5rem;
+    font-size: 1.6rem;
     text-align: center;
     padding: 0.5rem 0;
+  }
+
+  @media all and (min-width: 768px) {
+    /* -------------------------------------------------------------------------- */
+    /*                                   테블릿                                    */
+    /* -------------------------------------------------------------------------- */
+
+    padding: 2rem 2rem 0 2rem;
+    border-radius: 10px;
+    margin-bottom: 20rem;
+
+    img {
+      min-height: 500px;
+    }
+
+    span {
+      font-size: 2.5rem;
+    }
   }
 `;
--- lodics/components/Business/KeyBusiness/styles.js
+++ lodics/components/Business/KeyBusiness/styles.js
@@ -2,13 +2,26 @@ import styled from 'styled-components';
 import { palette } from '../../../styles/color';
 
 export const ImgContainer = styled.div`
+  /* -------------------------------------------------------------------------- */
+  /*                               모바일  & default                             */
+  /* -------------------------------------------------------------------------- */
   width: 100%;
-  padding: 0 5rem;
 
   img {
     border: 1px solid ${palette.themeDashedBorder};
     display: block;
     width: 100%;
-    min-height: 80vh;
+    min-height: 40vh;
+  }
+
+  @media all and (min-width: 768px) {
+    /* -------------------------------------------------------------------------- */
+    /*                                   테블릿                                    */
+    /* -------------------------------------------------------------------------- */
+    padding: 0 5rem;
+
+    img {
+      min-height: 80vh;
+    }
   }
 `;
