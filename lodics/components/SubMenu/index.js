4b886b16cfac6c72d6cd2173a10939ce5ce27372
윤민지 <ymj210@lodics.com>
2022-01-05 18:21:46 +0900

    fix/submenu 수정 (context 관련 디버깅 필요)

--- lodics/components/SubMenu/index.js
+++ lodics/components/SubMenu/index.js
@@ -1,9 +1,10 @@
-import React, { useCallback } from 'react'
+import React, { useContext, useCallback, useEffect, useState } from 'react'
 import { Container, SubContainer } from './styles'
 import { Select, Button } from 'antd'
 import 'antd/dist/antd.css' // or 'antd/dist/antd.less'
 import { HomeOutlined } from '@ant-design/icons'
 import Router from 'next/router'
+import GlobalStateContext from '../../context/globalStateContext'
 
 const { Option } = Select
 
@@ -14,14 +15,39 @@ const mainSelect = [
   { value: 'recruitment', text: 'Recruitment' },
 ]
 
-export default function SubMenu({ title }) {
+export default function SubMenu({ title, subSelectList = [] }) {
+  const [newSubSelectList, setNewSubSelectList] = useState(subSelectList)
+  const [currentSelect, setCurrentSelect] = useState('')
+
+  const { setSubSelectedComponent } = useContext(GlobalStateContext)
+
   const onClickGoHome = useCallback(() => {
     Router.push('/')
   })
 
-  const creteSelect = (defaultValue, selectList) => {
+  const onChangeSelect = useCallback(value => {
+    console.log(value)
+    console.log(setSubSelectedComponent)
+    setCurrentSelect(value)
+    if (
+      value === 'company' ||
+      value === 'business' ||
+      value === 'productsServices' ||
+      value === 'recruitment'
+    ) {
+      Router.push(`/${value}`)
+    } else {
+      setSubSelectedComponent(value)
+    }
+  }, [])
+
+  const createSelect = (defaultValue, selectList, width = 250) => {
     return (
-      <Select defaultValue={defaultValue}>
+      <Select
+        defaultValue={defaultValue}
+        style={{ width: width }}
+        onChange={onChangeSelect}
+      >
         {selectList.map(({ value, text }) => (
           <Option key={value} value={value}>
             {text}
@@ -30,6 +56,14 @@ export default function SubMenu({ title }) {
       </Select>
     )
   }
+
+  useEffect(() => {
+    if (currentSelect === 'keyTechnology') {
+      setNewSubSelectList(subSelectList)
+    } else if (currentSelect === 'eGoverment') {
+      setNewSubSelectList([subSelectList[0]])
+    }
+  }, [currentSelect])
 
   return (
     <>
@@ -44,7 +78,15 @@ export default function SubMenu({ title }) {
           size={'large'}
         />
 
-        {creteSelect(title, mainSelect)}
+        {createSelect(title, mainSelect)}
+        {!!newSubSelectList.length &&
+          newSubSelectList.map(subSelect =>
+            createSelect(
+              subSelect[0]['text'],
+              subSelect,
+              subSelect[0]['value'] === 'keyTechnology01' ? 400 : 200
+            )
+          )}
       </SubContainer>
     </>
   )
