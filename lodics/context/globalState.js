17879360a055ac9e6fda9d468e4fa36f66df51ac
윤민지 <ymj210@lodics.com>
2022-01-05 18:21:06 +0900

    fix/context 수정

--- lodics/context/globalState.js
+++ lodics/context/globalState.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GlobalStateContext from './globalStateContext'
 
 export default function GlobalState({ children }) {
   const [subSelectedComponent, setSubSelectedComponent] = useState('')
+  const [selectedSubMenu, setSelectedSubMenu] = useState('')
+
   return (
     <GlobalStateContext.Provider
       value={{
         subSelectedComponent,
         setSubSelectedComponent,
+        selectedSubMenu,
+        setSelectedSubMenu,
       }}
     >
       {children}
