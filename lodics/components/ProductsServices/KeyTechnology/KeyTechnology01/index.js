0f4c0a3e81123b74debb003b4d516084e73752d3
윤민지 <ymj210@lodics.com>
2022-01-05 11:50:05 +0900

    feat/핵심 기술 page 구현

--- lodics/components/ProductsServices/KeyTechnology/KeyTechnology01/index.js
+++ lodics/components/ProductsServices/KeyTechnology/KeyTechnology01/index.js
@@ -1,10 +1,121 @@
 import React from 'react'
-import { WrapContents, Contents } from './styles'
+import { WrapContents, Contents, Contents_, Div, Div_ } from './styles'
 
 export default function KeyTechnology01() {
   return (
     <>
       <WrapContents>
+        <Div>
+          <span>{'GEOINT\n자료 통합처리 기술'}</span>
+          <ul>
+            <li>{'다중센서\n융합처리'}</li>
+            <li>{'GEOINT\n정보추출'}</li>
+            <li>{'분산형\nGEOINT DB\n통합처리'}</li>
+            <li>{'정보융합\n분산 정보처리'}</li>
+          </ul>
+        </Div>
+        <Div_>
+          <span>{'GEOINT\n자료 통합처리 기술'}</span>
+          <div>
+            <ul>
+              <li>
+                입체시 Viewer
+                <table>
+                  <tr>
+                    <td>여색입체</td>
+                    <td>편광입체</td>
+                  </tr>
+                </table>
+              </li>
+              <li>Stereo 영상 생성</li>
+              <li>
+                군사 규격 자료
+                <table>
+                  <tr>
+                    <td colSpan={2}>CIB</td>
+                  </tr>
+                  <tr>
+                    <td>DTED</td>
+                    <td>GARS</td>
+                  </tr>
+                  <tr>
+                    <td>DPPDB</td>
+                    <td>CADRG</td>
+                  </tr>
+                </table>
+              </li>
+              <li>
+                NITF
+                <table>
+                  <tr>
+                    <td>NITF 2.0</td>
+                    <td>NITF 2.1</td>
+                  </tr>
+                  <tr>
+                    <td>TRE 처리</td>
+                    <td>압축영상 처리</td>
+                  </tr>
+                </table>
+              </li>
+              <li>센서 모델링</li>
+            </ul>
+            <ul>
+              <li>
+                RPC/RSM Generator
+                <table>
+                  <tr>
+                    <td>{'RSM\nExploiter'}</td>
+                    <td>{'RSM\nGenerator'}</td>
+                  </tr>
+                </table>
+              </li>
+              <li>
+                기하/방사 보정
+                <table>
+                  <tr>
+                    <td>단영상 모델링</td>
+                  </tr>
+                  <tr>
+                    <td>Block Modeling</td>
+                  </tr>
+                  <tr>
+                    <td>정확도 자료 생성</td>
+                  </tr>
+                </table>
+              </li>
+              <li>
+                CSM
+                <table>
+                  <tr>
+                    <td>Pushbroom</td>
+                    <td>Whiskbroom</td>
+                  </tr>
+                  <tr>
+                    <td>Frame</td>
+                    <td>SAR</td>
+                  </tr>
+                  <tr>
+                    <td>DLT</td>
+                    <td>RPC/RSM</td>
+                  </tr>
+                </table>
+              </li>
+              <li>특징점 추출 및 매칭</li>
+            </ul>
+          </div>
+        </Div_>
+        <Div>
+          <span>{'위치기반\n통합 모니터링 기술'}</span>
+          <ul>
+            <li>가시영역 처리</li>
+            <li>이동제 위치추적</li>
+            <li>GIS 자료 중첩도시</li>
+            <li>3D 지형처리</li>
+            <li>PostgreSQL/PostGIS</li>
+          </ul>
+        </Div>
+      </WrapContents>
+      {/* <WrapContents>
         <Contents>
           <span>{'GEOINT\n자료 통합처리 기술'}</span>
           <div>{'다중센서\n융합처리'}</div>
@@ -12,21 +123,39 @@ export default function KeyTechnology01() {
           <div>{'분산형\nGEOINT DB\n통합처리'}</div>
           <div>{'정보융합\n분산 정보처리'}</div>
         </Contents>
+        <Contents_>
+          <span>{'다중영상기반 정밀 위치결정자료 제작 기술'}</span>
+          <div>
+            <div>
+              <div>
+                입체시 Viewer
+                <div>
+                  <div>여색입체</div>
+                  <div>여색입체</div>
+                </div>
+              </div>
+              <div>Stereo 영상 생성</div>
+              <div>군사 규격 자료</div>
+              <div>NITF</div>
+              <div>센서 모델링</div>
+            </div>
+            <div>
+              <div>RPC/RSM Generator</div>
+              <div>기하/방사 보정</div>
+              <div>CSM</div>
+              <div>특징점 추출 및 매칭</div>
+            </div>
+          </div>
+        </Contents_>
         <Contents>
-          <span>{'GEOINT\n자료 통합처리 기술'}</span>
-          <div>다중센서 융합처리</div>
-          <div>다중센서 융합처리</div>
-          <div>다중센서 융합처리</div>
-          <div>다중센서 융합처리</div>
+          <span>{'위치기반\n통합 모니터링 기술'}</span>
+          <div>가시영역 처리</div>
+          <div>이동제 위치추적</div>
+          <div>GIS 자료 중첩도시</div>
+          <div>3D 지형처리</div>
+          <div>PostgreSQL/PostGIS</div>
         </Contents>
-        <Contents>
-          <span>{'GEOINT\n자료 통합처리 기술'}</span>
-          <div>다중센서 융합처리</div>
-          <div>다중센서 융합처리</div>
-          <div>다중센서 융합처리</div>
-          <div>다중센서 융합처리</div>
-        </Contents>
-      </WrapContents>
+      </WrapContents> */}
     </>
   )
 }
--- lodics/components/ProductsServices/KeyTechnology/KeyTechnology01/styles.js
+++ lodics/components/ProductsServices/KeyTechnology/KeyTechnology01/styles.js
@@ -7,11 +7,83 @@ export const WrapContents = styled.div`
   justify-content: space-between;
 `
 
+export const Div = styled.div`
+  border: 1px solid #dddddd;
+  border-radius: 10px;
+  text-align: center;
+  span {
+    font-size: 20px;
+    font-weight: 500;
+    white-space: pre-wrap;
+    color: ${palette.themePrimary};
+  }
+  ul {
+    list-style: none;
+    padding: 0px;
+    li {
+      background-color: #f6f6f6;
+      border: 1px solid #dddddd;
+      border-radius: 2px;
+      padding: 40px 0px;
+      width: 200px;
+      margin: 10px 10px 10px 10px;
+      color: #464646;
+      font-size: 18px;
+      font-weight: 500;
+      white-space: pre-wrap;
+    }
+  }
+`
+
+export const Div_ = styled.div`
+  border: 1px solid #dddddd;
+  border-radius: 10px;
+  text-align: center;
+  padding: 0px 5px 0px 5px;
+  margin: 0px 10px;
+  span {
+    font-size: 20px;
+    font-weight: 500;
+    white-space: pre-wrap;
+    color: ${palette.themePrimary};
+  }
+  div {
+    display: flex;
+    flex-direction: row;
+    ul {
+      list-style: none;
+      padding: 0px;
+      li {
+        background-color: #f6f6f6;
+        border: 1px solid #dddddd;
+        border-radius: 2px;
+        padding: 10px 0px 10px 0px;
+        margin: 10px 5px 10px 5px;
+        color: #464646;
+        font-size: 18px;
+        font-weight: 500;
+        white-space: pre-wrap;
+        table {
+          border-collapse: separate;
+          border-spacing: 10px 10px;
+        }
+
+        td {
+          background-color: ${palette.themeWhite};
+          border: 1px solid #dddddd;
+          padding: 5px 0px;
+          width: 300px;
+        }
+      }
+    }
+  }
+`
+
 export const Contents = styled.div`
   border: 1px solid #dddddd;
   border-radius: 10px;
   text-align: center;
-  padding: 10px 10px 10px 10px;
+  padding: 10px 0px 10px 0px;
 
   span {
     font-size: 20px;
@@ -23,12 +95,69 @@ export const Contents = styled.div`
   div {
     background-color: #f6f6f6;
     border: 1px solid #dddddd;
-    border-radius: 5px;
-    padding: 40px 60px 40px 60px;
+    border-radius: 2px;
+    padding: 40px 70px 40px 70px;
     margin: 10px 10px 10px 10px;
     color: #464646;
     font-size: 18px;
     font-weight: 500;
     white-space: pre-wrap;
   }
+`
+
+export const Contents_ = styled.div`
+  border: 1px solid #dddddd;
+  border-radius: 10px;
+  text-align: center;
+  padding: 10px 0px 10px 0px;
+
+  span {
+    font-size: 20px;
+    font-weight: 500;
+    white-space: pre-wrap;
+    color: ${palette.themePrimary};
+  }
+
+  div {
+    display: flex;
+    flex-direction: row;
+    div {
+      display: flex;
+      flex-direction: column;
+      div {
+        width: 15em;
+        background-color: #f6f6f6;
+        border: 1px solid #dddddd;
+        border-radius: 2px;
+        /* padding: 40px 70px 40px 70px;
+        margin: 10px 10px 10px 10px; */
+        color: #464646;
+        font-size: 18px;
+        font-weight: 500;
+        white-space: pre-wrap;
+        div {
+          display: flex;
+          flex-direction: row;
+          padding: 0px 0px 0px 0px;
+          div {
+            background-color: ${palette.themeWhite};
+            /* margin: 0px auto; */
+            padding: 0px 0px 0px 0px;
+          }
+        }
+      }
+    }
+  }
+
+  /* div + div {
+    background-color: #f6f6f6;
+    border: 1px solid #dddddd;
+    border-radius: 2px;
+    padding: 40px 70px 40px 70px;
+    margin: 10px 10px 10px 10px;
+    color: #464646;
+    font-size: 18px;
+    font-weight: 500;
+    white-space: pre-wrap;
+  } */
 `
