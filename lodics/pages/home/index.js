b59a5befd1ef6266cab7a6596c28be1e67b90e57
jihye <jjh@lodics.com>
2022-01-06 09:57:34 +0900

    refactor/IntroText 애니메이션 적용 및 리랜더링 방지를 위한 컴포넌트 분리

--- lodics/pages/home/index.js
+++ lodics/pages/home/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { AppLayout, Card } from '../../components';
-import { CardContainer, Container, TextContainer } from './styles';
+import IntroText from './IntroText';
+import { CardContainer, Container, Background } from './styles';
 
 export default function Home() {
   useEffect(() => {
@@ -10,11 +11,8 @@ export default function Home() {
   return (
     <Container>
       <AppLayout theme='white'>
-        <TextContainer>
-          <span>Leaders of GEOINT</span>
-          <span>로딕스는 특수분야의 사업수행 경험을 바탕으로</span>
-          <span>센서기반 공간정보 응용 솔루션 개발, GIS 솔루션 시장, GIS DB 구축 사업을 추진합니다.</span>
-        </TextContainer>
+        <Background />
+        <IntroText />
         <CardContainer>
           <Card menu='Business' description='LoDiCS의 사업분야를 소개합니다' link='business' />
           <Card menu='Producs & Services' description='LoDiCS의 보유기술을 소개합니다' link='productsServices' />
--- lodics/pages/home/styles.js
+++ lodics/pages/home/styles.js
@@ -2,18 +2,34 @@ import styled from 'styled-components';
 import { palette } from '../../styles/color';
 
 export const Container = styled.div`
-  /* -------------------------------------------------------------------------- */
-  /*                               모바일  & default                             */
-  /* -------------------------------------------------------------------------- */
   min-height: 100vh;
   overflow: hidden;
-  background: left / cover no-repeat url('img/basic_bg.jpg');
+  position: relative;
+`;
 
-  @media all and (min-width: 768px) {
-    /* -------------------------------------------------------------------------- */
-    /*                                   테블릿                                    */
-    /* -------------------------------------------------------------------------- */
-    background: center / cover no-repeat url('img/basic_bg.jpg');
+export const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -5;
+  background: center / cover no-repeat url('img/basic_bg.jpg');
+  animation: scaleAnimation 10s cubic-bezier(0.4, 0.4, 0.4, 1) 0s infinite normal forwards;
+
+  @keyframes scaleAnimation {
+    0% {
+      transform: scale(1.4);
+      filter: blur(1px);
+    }
+    20% {
+      filter: blur(0);
+    }
+    95% {
+      filter: brightness(1);
+    }
+    100% {
+      transform: scale(1);
+      filter: brightness(1.1);
+    }
   }
 `;
 
@@ -27,19 +43,62 @@ export const TextContainer = styled.div`
   flex-flow: column;
   backdrop-filter: grayscale(50%);
   margin: 10rem auto;
+  min-height: 18rem;
+  position: relative;
 
-  span {
-    font-size: 3rem;
-    font-weight: 600;
-    margin-bottom: 4rem;
-    text-align: center;
+  .first_line,
+  .second_line {
     color: ${palette.themeWhite};
+    text-align: center;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100%;
   }
 
-  span + span {
+  .first_line {
+    font-size: 3rem;
+    font-weight: 600;
+
+    animation: firstTextAnimation 10s ease-out 0s infinite normal forwards;
+    @keyframes firstTextAnimation {
+      0% {
+        opacity: 0;
+        top: -50%;
+      }
+      10% {
+        opacity: 1;
+        top: 0;
+      }
+    }
+  }
+
+  .second_line {
     font-size: 1.6rem;
     font-weight: 300;
     margin-bottom: 0;
+  }
+
+  .second_line {
+    animation: secondTextAnimation 10s ease-out 0s infinite normal forwards;
+    @keyframes secondTextAnimation {
+      0% {
+        opacity: 0;
+        top: 0%;
+      }
+      10% {
+        opacity: 0;
+        top: 0%;
+      }
+      20% {
+        opacity: 1;
+        top: 50%;
+      }
+      100% {
+        opacity: 1;
+        top: 50%;
+      }
+    }
   }
 
   @media all and (min-width: 768px) {
@@ -48,11 +107,13 @@ export const TextContainer = styled.div`
     /* -------------------------------------------------------------------------- */
     width: calc(100% - 2rem);
 
-    span {
+    .first_line {
+      white-space: nowrap;
       font-size: 3.2rem;
     }
 
-    span + span {
+    .second_line {
+      white-space: nowrap;
       font-size: 2rem;
       margin-top: 1rem;
     }
