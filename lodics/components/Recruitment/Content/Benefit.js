609529fd60378945e22c63a0cca2055875dfb77c
윤민지 <ymj210@lodics.com>
2022-01-12 10:46:21 +0900

    style/채용 복리후생 style 수정

--- lodics/components/Recruitment/Content/Benefit.js
+++ lodics/components/Recruitment/Content/Benefit.js
@@ -3,6 +3,95 @@ import { PageSemiTitle } from 'components';
 import { palette } from '@styles/color';
 import { Line, Circle, WrapCirCle } from '../styles';
 
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
+
+const images = [
+  {
+    url: 'img/recruitment/benefit01.png',
+    title: '복지제도',
+    width: '33%',
+    contents: '4대보험 가입\n청년내일채움공제 가입\n경조금 및 경조 휴가 부여',
+  },
+  {
+    url: 'img/recruitment/benefit02.png',
+    title: '인재육성',
+    width: '33%',
+    contents: '수시교육\n도서 구입비 지원',
+  },
+  {
+    url: 'img/recruitment/benefit03.png',
+    title: '여가활동',
+    width: '33%',
+    contents: '주 40시간제 실시\n상시 휴가제 운영\n사원들의 여가활동 보장',
+  },
+];
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: 'relative',
+  height: 200,
+  [theme.breakpoints.down('sm')]: {
+    width: '100% !important', // Overrides inline-style
+    height: 100,
+  },
+  '&:hover, &.Mui-focusVisible': {
+    zIndex: 1,
+    '& .MuiImageBackdrop-root': {
+      opacity: 0.15,
+    },
+    '& .MuiImageMarked-root': {
+      opacity: 0,
+    },
+    '& .MuiTypography-root': {
+      border: '4px solid currentColor',
+    },
+  },
+}));
+
+const ImageSrc = styled('span')({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center 40%',
+});
+
+const Image = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.common.white,
+}));
+
+const ImageBackdrop = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create('opacity'),
+}));
+
+const ImageMarked = styled('span')(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: 'absolute',
+  bottom: -2,
+  left: 'calc(50% - 9px)',
+  transition: theme.transitions.create('opacity'),
+}));
+
 const benefitList = [
   {
     title: '복지제도',
@@ -43,7 +132,42 @@ export default function Benefit() {
     <>
       <Line />
       <PageSemiTitle title='복리후생' color={palette.themeBlack} fontWeight={400} />
-      <WrapCirCle>{benefitContent(benefitList)}</WrapCirCle>
+      <Box sx={{ marginTop: 5, display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
+        {images.map(image => (
+          <ImageButton
+            focusRipple
+            key={image.title}
+            style={{
+              width: image.width,
+              height: 300,
+              margin: 2,
+            }}
+          >
+            <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+            <ImageBackdrop className='MuiImageBackdrop-root' />
+            <Image style={{ display: 'flex', flexDirection: 'column' }}>
+              <Typography
+                component='span'
+                variant='subtitle1'
+                color='inherit'
+                sx={{
+                  fontSize: 30,
+                  fontWeight: 'bold',
+                  position: 'relative',
+                  p: 4,
+                  pt: 2,
+                  pb: theme => `calc(${theme.spacing(1)} + 6px)`,
+                }}
+              >
+                {image.title}
+                <ImageMarked className='MuiImageMarked-root' />
+              </Typography>
+              <div style={{ marginTop: 25, whiteSpace: 'pre-wrap', fontSize: 15 }}>{image.contents}</div>
+            </Image>
+          </ImageButton>
+        ))}
+      </Box>
+      {/* <WrapCirCle>{benefitContent(benefitList)}</WrapCirCle> */}
     </>
   );
 }
