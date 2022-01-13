9a7d2d0a0dafc2cbe20f39243db4424696148382
jihye <jjh@lodics.com>
2022-01-13 12:26:01 +0900

    fix/ Products&Services 페이지 select 오류 해결

--- lodics/components/SubMenu/index.js
+++ lodics/components/SubMenu/index.js
@@ -66,6 +66,19 @@ export default function SubMenu({ title, subSelectList = [] }) {
     }
   };
 
+  // option이 열려 있을 때 다른 페이지로 이동 시 닫기
+  useEffect(() => {
+    return () => {
+      setSelectedSubMenu('');
+      setIsOpen(false);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (selectedMenu === 'productsServices' && !currentSelect) setNewSubSelectList([subSelectList[0]]);
+    else setNewSubSelectList(subSelectList);
+  }, [language]);
+
   useEffect(() => {
     if (!!subSelectList.length) {
       if (
@@ -81,18 +94,9 @@ export default function SubMenu({ title, subSelectList = [] }) {
     }
   }, [currentSelect]);
 
-  // option이 열려 있을 때 다른 페이지로 이동 시 닫기
   useEffect(() => {
-    return () => {
-      setSelectedSubMenu('');
-      setIsOpen(false);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (selectedMenu === 'productsServices' && !currentSelect) setNewSubSelectList([subSelectList[0]]);
-    else setNewSubSelectList(subSelectList);
-  }, [language]);
+    if (selectedMenu === 'productsServices') setNewSubSelectList([subSelectList[0]]);
+  }, [selectedMenu]);
 
   return (
     <Container>
@@ -110,7 +114,7 @@ export default function SubMenu({ title, subSelectList = [] }) {
           {!!newSubSelectList.length &&
             newSubSelectList.map((subSelect, i) => (
               <Select
-                defaultValue={subSelect[0]['text']}
+                defaultValue={subSelect?.[0]['text']}
                 selectList={subSelect}
                 onClickSelect={onClickSelect}
                 selectNumber={i + 1}
